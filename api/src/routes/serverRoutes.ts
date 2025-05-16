5d207fbe79afd58f6c4af06a04f2a72458fc154e
Refrizor <retizao.tv@gmail.com>
2025-05-15 21:20:49 -0400

    Remove old routes

--- api/src/routes/serverRoutes.ts
+++ api/src/routes/serverRoutes.ts
@@ -5,10 +5,8 @@ const router = Router();
 
 router.get('/servers/:uuid', serverController.getServer);
 router.get('/servers', serverController.getAllServers);
-// router.get('/servers/players', serverController.fetchPlayers);
 
 router.post('/servers', serverController.registerServer);
 router.patch('/servers/:uuid', serverController.updateServer);
-// router.put('/servers/:uuid/playercount', serverController.updatePlayerCount);
 
 export default router;
